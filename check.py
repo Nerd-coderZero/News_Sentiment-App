ec0a02e3641c4af1adb4d7302f4bc2671e5460c4
Kushagra Jaiswal <61771073+Nerd-coderZero@users.noreply.github.com>
2025-03-24 06:24:51 +0530

    Delete check.py

--- check.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-api_key = "AIzaSyDx8DT-eQYGYEs1aOZnkgwM81QyMEnWreg"
-genai.configure(api_key=api_key)
-
-model = genai.GenerativeModel('gemini-2.0-flash')
-response = model.generate_content("Hello, how are you?")
-print(response.text)
